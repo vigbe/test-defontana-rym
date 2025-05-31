146ad82626384397f007ebb41b05579a4ca937ec
vigbe <victor.bastiasescobar@gmail.com>
2025-05-31 13:59:25 -0400

    proyecto funciona correctamente, merge con standAlone branch

--- src/app/components/characters-list/characters-list.component.ts
+++ src/app/components/characters-list/characters-list.component.ts
@@ -1,28 +1,45 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
-// Acá importaos los servicios de Character y Favorites
 import { CharacterService } from '../../services/character.service';
 import { FavoritesService } from '../../services/favorites.service';
-import { NgFor } from '@angular/common';
-import { MatTable } from '@angular/material/table';
 import { SearchFiltersComponent } from '../search-filters/search-filters.component';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-characters-list',
-    templateUrl: './characters-list.component.html',
-    styleUrls: ['./characters-list.component.scss'],
-    standalone: true,
-    imports: [SearchFiltersComponent, MatTable, NgFor, CommonModule]
+  selector: 'app-characters-list',
+  templateUrl: './characters-list.component.html',
+  styleUrls: ['./characters-list.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    SearchFiltersComponent,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class CharactersListComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'status', 'species', 'type', 'gender', 'created'];
+  displayedColumns: string[] = [
+    'name',
+    'status',
+    'species',
+    'type',
+    'gender',
+    'created',
+    'favorite' // <-- Solo si tienes la columna de favoritos en el HTML
+  ];
   characters: any[] = [];
-  filters = { name: '', status: '', species: '', type: '', gender : '', created: '' };
+  filters = { name: '', status: '', species: '', type: '', gender: '', created: '' };
 
-@Output() charactersChange = new EventEmitter<any[]>();
-@Output() characterSelected = new EventEmitter<any>();
+  @Output() charactersChange = new EventEmitter<any[]>();
+  @Output() characterSelected = new EventEmitter<any>();
 
-  constructor(private characterService: CharacterService, private favoritesService: FavoritesService) {}
+  constructor(
+    private characterService: CharacterService,
+    private favoritesService: FavoritesService
+  ) {}
 
   ngOnInit() {
     this.loadCharacters();
