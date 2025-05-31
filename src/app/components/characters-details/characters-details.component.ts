4cf833d0b266ef024039de09c6e7454969935988
vigbe <victor.bastiasescobar@gmail.com>
2025-05-30 20:30:55 -0400

    creacion de componentes y carpetas

---
+++ src/app/components/characters-details/characters-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './characters-details.component.html',
+  styleUrl: './characters-details.component.scss'
+})
+export class CharactersDetailsComponent {
+
+}
---
+++ src/app/components/totals/totals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-totals',
+  standalone: true,
+  imports: [],
+  templateUrl: './totals.component.html',
+  styleUrl: './totals.component.scss'
+})
+export class TotalsComponent {
+
+}
