b1a212be3bef26727a1a9391f4060de91606724e
vigbe <victor.bastiasescobar@gmail.com>
2025-05-31 22:04:32 -0400

    actualizar vite y otros para una url mal creada por caracteres inpropios

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,5 +1,3 @@
-//este será el servicio que se encargará de las peticiones HTTP relacionadas con los personajes
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,26 +8,29 @@ import { environment } from '../../environments/environment';
 })
 export class CharacterService {
   private apiUrl = `${environment.baseUrlAPI}/character`;
+
   constructor(private http: HttpClient) { }
-  // Método para obtener todos los personajes
-    getCharacters(filters: any = {}): Observable<any> {
+
+  // Obtener todos los personajes con filtros
+  getCharacters(filters: any = {}): Observable<any> {
     let params = new HttpParams();
-    // Añadir filtros a los parámetros de la solicitud
     Object.keys(filters).forEach(key => {
       if (filters[key]) params = params.set(key, filters[key]);
     });
-    // Realizar la solicitud GET a la API
     return this.http.get(this.apiUrl, { params });
   }
-  // Método para obtener un personaje específico por ID
+
+  // Obtener un personaje específico por ID
   getCharacter(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
-  // Métodos para obtener ubicaciones y episodios
+
+  // Obtener ubicación por URL
   getLocation(url: string): Observable<any> {
     return this.http.get(url);
   }
-  // Método para obtener un episodio específico por URL
+
+  // Obtener episodio por URL
   getEpisode(url: string): Observable<any> {
     return this.http.get(url);
   }
